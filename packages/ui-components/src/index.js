db1d65b0e9332256f0929028a302c7b11e50d4ff
Clara M <claramm94@gmail.com>
2021-04-15 09:59:47 +0200

    update components

--- packages/ui-components/src/index.js
+++ packages/ui-components/src/index.js
@@ -1,3 +1,20 @@
-import Message from './components/Message';
+import React, { useState } from 'react'
+import Button from './components/Button';
+import styles from './styles.module.css'
 
-export { Message };
+const Message = () => {
+  const [showMessage, setShowMessage] = useState(false);
+
+  const handleClick = () => {
+    setShowMessage(!showMessage);
+  }
+
+  return (
+    <div className={styles.test}>
+      <Button label='Testing Lerna' onClick={handleClick}/>
+      {showMessage && <span>Lerna Demo</span>}
+    </div>
+  );
+};
+
+export default Message;
