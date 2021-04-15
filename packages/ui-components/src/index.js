cb8d19ddd1f7fd54c839c2198384d1e9a6c49819
Clara M <claramm94@gmail.com>
2021-04-15 10:08:13 +0200

    update message

--- packages/ui-components/src/index.js
+++ packages/ui-components/src/index.js
@@ -2,19 +2,11 @@ import React, { useState } from 'react'
 import Button from './components/Button';
 import styles from './styles.module.css'
 
-const Message = () => {
-  const [showMessage, setShowMessage] = useState(false);
-
-  const handleClick = () => {
-    setShowMessage(!showMessage);
-  }
-
-  return (
-    <div className={styles.test}>
-      <Button label='Testing Lerna' onClick={handleClick}/>
-      {showMessage && <span>Lerna Demo</span>}
-    </div>
-  );
-};
+const Message = () => (
+  <div className={styles.test}>
+    <Button label='Testing Lerna' onClick={handleClick}/>
+    <span>Lerna Demo</span>
+  </div>
+);
 
 export default Message;
